779f4ecfc7320ce841fe55ec24159b0a04e42ca6
Neo Sekhukhune <neotron1625@gmail.com>
2023-03-27 15:40:43 -0700

    First attempt

--- main.h
+++ main.h
@@ -1,5 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdarg.h>
+#include <unistd.h>
+
+int _printc(va_list c);
+int _prints(va_list s);
+int _printl(void);
+int _printf(const char *format, ...);
+
 
 #endif /*MAIN_H*/
---
+++ printf.c
@@ -0,0 +1,115 @@
+#include "main.h"
+#include <stdarg.h>
+#include <unistd.h>
+#include <string.h>
+
+int _printc(va_list c);
+int _prints(va_list s);
+int _printl(void);
+int _printf(const char *format, ...);
+
+/**
+ * _printc - Prints a character
+ * @c: Argument list.
+ *
+ * Return: Returns the number of characters printed.
+ */
+int _printc(va_list c)
+{
+	char ret;
+
+	ret = va_arg(c, int);
+
+	write(STDOUT_FILENO, &ret, 1);
+
+	return (1);
+}
+
+/**
+ * _printl - Function that prints '%'.
+ *
+ * Return: Return number of characters printed.
+ */
+int _printl(void)
+{
+	char per;
+
+	per = '%';
+
+	write(STDOUT_FILENO, &per, 1);
+
+	return (1);
+}
+
+/**
+ * _prints - Function that prints a string
+ * @s: Argument list
+ *
+ * Return: Returns the number of characters printed.
+ */
+int _prints(va_list s)
+{
+	char *str;
+
+	str = va_arg(s, char *);
+
+	if (str == NULL)
+	{
+		return (0);
+	}
+
+	write(STDOUT_FILENO, str, strlen(str));
+
+	return (strlen(str));
+}
+
+
+
+/**
+ * _printf - Function that produces output accordong to a format.
+ * @format: Character string.
+ *
+ * Return: Returns the number of characters printed
+ */
+int _printf(const char *format, ...)
+{
+
+	int i, total_chars_printed = 0;
+
+	va_list a_list;
+
+	va_start(a_list, format);
+
+	i = 0;
+	while (format != NULL && format[i] != '\0')
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			if (format[i] == 'c')
+			{
+				total_chars_printed += _printc(a_list);
+			}
+			if (format[i] == 's')
+			{
+				total_chars_printed += _prints(a_list);
+			}
+			if (format[i] == '%')
+			{
+				total_chars_printed += _printl();
+			}
+		}
+		else
+		{
+			write(STDOUT_FILENO, &format[i], 1);
+			total_chars_printed++;
+		}
+		i++;
+	}
+
+	va_end(a_list);
+
+	return (total_chars_printed);
+}
+
+
